8524d0d3f007070585794ae010ddeb7fb5664a0d
Annalee Switek <annaleejude@gmail.com>
2018-03-01 15:01:33 -0500

    commented out console.logs

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,9 @@
 function bubbleSort(array) {
   for(let i = 0; i < array.length -1; i++) {
-    console.log(array)
+    // console.log(array)
     for(let j = i+1; j < array.length; j++ ) {
       if (array[i] > array[j]) {
-        console.log(array[j])
+        // console.log(array[j])
         let temp = array[i];
 
         array[i] = array[j];
